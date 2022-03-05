619bed291866cd2e8c91f57f85d2234d6e7c2b20
Kevin Lacson <lacsonky@gmail.com>
2022-03-05 16:57:15 -0500

    rename controllers dir, set up user routes

--- server/routes/api/userRoutes.js
+++ server/routes/api/userRoutes.js
@@ -1,31 +1,29 @@
 const router = require("express").Router();
 
-// const {
-//     getUsers,
-//     getUserById,
-//     updateUser,
-//     deleteUser,
-//     createUser,
-//     addFriend,
-//     deleteFriend,
-// } = require("../../controllers/userController");
+const {
+    getUsers,
+    getUserById,
+    updateUser,
+    deleteUser,
+    createUser,
+} = require("../../controllers/userController");
 
-// router
-//     // route: /api/users
-//     .route("/")
-//     // GET: get all users
-//     .get(getUsers)
-//     // PUT: create user
-//     .post(createUser);
+router
+    // route: /api/users
+    .route("/")
+    // GET: get all users
+    .get(getUsers)
+    // PUT: create user
+    .post(createUser);
 
-// router
-//     // route: /api/users/:userId
-//     .route("/:userId")
-//     // GET: user by id
-//     .get(getUserById)
-//     // PUT: update user
-//     .put(updateUser)
-//     // DELETE: delete user
-//     .delete(deleteUser);
+router
+    // route: /api/users/:userId
+    .route("/:userId")
+    // GET: user by id
+    .get(getUserById)
+    // PUT: update user
+    .put(updateUser)
+    // DELETE: delete user
+    .delete(deleteUser);
 
 module.exports = router;
