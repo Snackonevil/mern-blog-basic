41f2fdf738e02ffe78c53c2969967b39e4bf075e
Kevin Lacson <lacsonky@gmail.com>
2022-03-05 16:49:46 -0500

    post schema

--- server/models/Post.js
+++ server/models/Post.js
@@ -1,5 +1,30 @@
 const { Schema, model } = require("mongoose");
 
-const postSchema = new Schema();
+const postSchema = new Schema(
+    {
+        postText: {
+            type: String,
+            required: true,
+            minlength: 1,
+            maxlength: 280,
+        },
+        createdAt: {
+            type: Date,
+            get: formatDate,
+            default: Date.now,
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true,
+        },
+        id: false,
+    }
+);
 
 module.exports = model("Post", postSchema);
