b855cef0579ff0e5d5fd4013e2b64c6f4e556a6c
Kevin Lacson <lacsonky@gmail.com>
2022-03-09 17:45:58 -0500

    added icon dependency

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,13 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import Register from "./pages/Register";
+import Header from "./components/Header";
 import "./App.css";
 
 function App() {
     return (
         <Router>
-            <div>
-                <h1>This is a basic blog using the MERN stack</h1>
-            </div>
+            <Header />
             <Routes>
                 <Route path="/" element={<Dashboard />} />
                 <Route path="/login" element={<Login />} />
